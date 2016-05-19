8488bbae7172581ab22cf9092afd3f7e1cf98d4f
cFrost <cfrost@cfrost.net>
2016-05-19 21:48:07 +0800

    token获取页面

--- net-eulerform-web-core/src/main/java/net/eulerform/web/core/security/authentication/service/impl/ClientService.java
+++ net-eulerform-web-core/src/main/java/net/eulerform/web/core/security/authentication/service/impl/ClientService.java
@@ -34,8 +34,8 @@ public class ClientService extends BaseService implements IClientService, Client
         c.setAuthorizedGrantTypes(authorizedGrantTypes);
         c.setScope(scope);
         Set<String> ruri = new HashSet<>();
-        ruri.add("http://10.88.5.166:7070/efb/webapi/svn/workingcopy");
-        ruri.add("http://10.88.5.166:7070/efb/getToken.html");
+        ruri.add("http://localhost:8080/eulerform-demo/webapi/svn/workingcopy");
+        ruri.add("http://localhost:8080/eulerform-demo/getToken.html");
         c.setRegisteredRedirectUri(ruri);
         return c;
 //        Client client = this.clientDao.findClientByClientId(clientId);
