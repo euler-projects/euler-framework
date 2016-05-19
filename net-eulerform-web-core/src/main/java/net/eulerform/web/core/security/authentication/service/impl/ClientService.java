09d2c4166d145e8c04b8dd02613c617de2b8c2a6
cFrost <cfrost@cfrost.net>
2016-05-19 16:28:11 +0800

    Outh2初步配置成功

--- net-eulerform-web-core/src/main/java/net/eulerform/web/core/security/authentication/service/impl/ClientService.java
+++ net-eulerform-web-core/src/main/java/net/eulerform/web/core/security/authentication/service/impl/ClientService.java
@@ -1,5 +1,8 @@
 package net.eulerform.web.core.security.authentication.service.impl;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import net.eulerform.web.core.base.service.impl.BaseService;
 import net.eulerform.web.core.security.authentication.dao.IClientDao;
 import net.eulerform.web.core.security.authentication.entity.Client;
@@ -19,10 +22,25 @@ public class ClientService extends BaseService implements IClientService, Client
 
     @Override
     public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
-        Client client = this.clientDao.findClientByClientId(clientId);
-        if(client == null)
-            throw new ClientRegistrationException("Client \"" + clientId + "\" not found");
-        return client;
+        Client c = new Client();
+        c.setId(clientId);
+        c.setClientId(clientId);
+        Set<String> authorizedGrantTypes = new HashSet<>();
+        authorizedGrantTypes.add("authorization_code");
+        Set<String> scope = new HashSet<>();
+        scope.add("READ");
+        scope.add("WRITE");
+        scope.add("TRUST");
+        c.setAuthorizedGrantTypes(authorizedGrantTypes);
+        c.setScope(scope);
+        Set<String> ruri = new HashSet<>();
+        ruri.add("http://10.88.5.166:7070/efb/webapi/svn/workingcopy");
+        c.setRegisteredRedirectUri(ruri);
+        return c;
+//        Client client = this.clientDao.findClientByClientId(clientId);
+//        if(client == null)
+//            throw new ClientRegistrationException("Client \"" + clientId + "\" not found");
+//        return client;
     }    
     
 }
