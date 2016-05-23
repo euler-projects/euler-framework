a3124ef6c6b2a819c787dd511f13f0d710491972
cFrost <cfrost@cfrost.net>
2016-05-23 09:37:43 +0800

    统一的Rest接口异常处理

--- net-eulerform-web-core/src/main/java/net/eulerform/web/core/base/controller/rest/BaseRest.java
+++ net-eulerform-web-core/src/main/java/net/eulerform/web/core/base/controller/rest/BaseRest.java
@@ -1,5 +1,20 @@
 package net.eulerform.web.core.base.controller.rest;
 
+import net.eulerform.web.core.base.entity.RestResponseEntity;
+import net.eulerform.web.core.base.entity.RestResponseStatus;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public abstract class BaseRest {
-	
+    /**  
+     * 用于在程序发生异常时统一返回错误信息 
+     * @return  
+     */  
+    @ResponseBody
+    @ExceptionHandler({Exception.class})   
+    public RestResponseEntity<String> exception(Exception e) {
+        e.printStackTrace();
+        return new RestResponseEntity<>(RestResponseStatus.UNKNOWN_ERR);
+    }
 }
--- net-eulerform-web-core/src/main/java/net/eulerform/web/core/base/entity/RestResponseStatus.java
+++ net-eulerform-web-core/src/main/java/net/eulerform/web/core/base/entity/RestResponseStatus.java
@@ -2,10 +2,10 @@ package net.eulerform.web.core.base.entity;
 
 public class RestResponseStatus {
 	
-	public final static RestResponseStatus SUCCESS = new RestResponseStatus(0, "SUCCESS");
+	public final static RestResponseStatus OK = new RestResponseStatus(200, "OK");
 	public final static RestResponseStatus SERVICE_NOT_FOUND = new RestResponseStatus(404, "SERVICE_NOT_FOUND");
-	public final static RestResponseStatus RESROURCE_NOT_FOUND = new RestResponseStatus(-1, "RESROURCE_NOT_FOUND");
-	public final static RestResponseStatus UNKNOWN_ERR = new RestResponseStatus(-1000, "UNKNOWN_ERR");
+	public final static RestResponseStatus NO_CONTENT = new RestResponseStatus(204, "NO_CONTENT");
+	public final static RestResponseStatus UNKNOWN_ERR = new RestResponseStatus(-1, "UNKNOWN_ERR");
 	
 	private Integer statusCode;
 	private String statusInfo;
