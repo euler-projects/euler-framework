4bedaa7d16868dd1df29af226da69aef38f240a7
cFrost <cfrost@cfrost.net>
2016-05-26 11:39:36 +0800

    404REST用RestEndpoint标注

--- net-eulerform-web-core/src/main/java/net/eulerform/web/core/base/controller/ServiceNotFoundRest.java
+++ net-eulerform-web-core/src/main/java/net/eulerform/web/core/base/controller/ServiceNotFoundRest.java
@@ -1,15 +1,15 @@
 package net.eulerform.web.core.base.controller;
 
+import net.eulerform.web.core.annotation.RestEndpoint;
+import net.eulerform.web.core.base.entity.WebServiceResponse;
+import net.eulerform.web.core.base.entity.WebServiceResponseStatus;
+
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import net.eulerform.web.core.base.entity.WebServiceResponse;
-import net.eulerform.web.core.base.entity.WebServiceResponseStatus;
-
-@Controller
+@RestEndpoint
 @Scope("prototype")
 @RequestMapping("/")
 public class ServiceNotFoundRest extends BaseRest {
