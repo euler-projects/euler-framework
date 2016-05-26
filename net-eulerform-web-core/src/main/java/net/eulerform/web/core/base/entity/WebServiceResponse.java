29c6a279d485711dc30ce509f9d7f7c1c97ecce7
cFrost <cfrost@cfrost.net>
2016-05-26 15:47:45 +0800

    javaDoc

--- net-eulerform-web-core/src/main/java/net/eulerform/web/core/base/entity/WebServiceResponse.java
+++ net-eulerform-web-core/src/main/java/net/eulerform/web/core/base/entity/WebServiceResponse.java
@@ -9,13 +9,31 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * 用来作为Webservice的返回数据实体
+ * 
+ * @author cFrost
+ *
+ * @param <T>
+ *            返回数据类型,支持单个对象和容器<br>
+ *            例如Object和List&lt;Object&gt;均只需指定为WebServiceResponse&lt;Object&gt;
+ */
 @XmlRootElement
 public class WebServiceResponse<T> {
 
+    /**
+     * 新建空实体,状态代码默认为204 No Content
+     */
     public WebServiceResponse() {
         this.setStatusToNoContent();
     }
 
+    /**
+     * 新建单个对象实体,如果数据不为空则状态码为200 OK
+     * 
+     * @param data
+     *            返回数据
+     */
     public WebServiceResponse(T data) {
         if (data == null) {
             this.setStatusToNoContent();
@@ -25,6 +43,12 @@ public class WebServiceResponse<T> {
         this.setStatus(HttpStatus.OK);
     }
 
+    /**
+     * 新建容器实体,如果数据不为空则状态码为200 OK
+     * 
+     * @param data
+     *            返回数据容器
+     */
     public WebServiceResponse(List<T> data) {
         if (data == null || data.isEmpty()) {
             this.setStatusToNoContent();
@@ -34,32 +58,76 @@ public class WebServiceResponse<T> {
         this.setStatus(HttpStatus.OK);
     }
 
+    /**
+     * 新建单个对象实体,并指定非标准HTTP状态码{@link WebServiceResponseStatus}
+     * 
+     * @param data
+     *            返回数据
+     * @param webServiceResponseStatus
+     *            非标准HTTP状态码
+     */
     public WebServiceResponse(T data, WebServiceResponseStatus webServiceResponseStatus) {
         this.setData(data);
         this.setStatus(webServiceResponseStatus);
     }
 
+    /**
+     * 新建容器实体,并指定非标准HTTP状态码{@link WebServiceResponseStatus}
+     * 
+     * @param data
+     *            返回数据容器
+     * @param webServiceResponseStatus
+     *            非标准HTTP状态码
+     */
     public WebServiceResponse(List<T> data, WebServiceResponseStatus webServiceResponseStatus) {
         this.setData(data);
         this.setStatus(webServiceResponseStatus);
     }
 
+    /**
+     * 新建单个对象实体,并指定标准HTTP状态码{@link HttpStatus}
+     * 
+     * @param data
+     *            返回数据
+     * @param httpStatus
+     *            标准HTTP状态码
+     */
     public WebServiceResponse(T data, HttpStatus httpStatus) {
         this.setData(data);
         this.setStatus(httpStatus);
     }
 
+    /**
+     * 新建容器实体,并指定标准HTTP状态码{@link HttpStatus}
+     * 
+     * @param data
+     *            返回数据容器
+     * @param httpStatus
+     *            标准HTTP状态码
+     */
     public WebServiceResponse(List<T> data, HttpStatus httpStatus) {
         this.setData(data);
         this.setStatus(httpStatus);
     }
 
+    /**
+     * 新建空实体,并指定标准HTTP状态码{@link HttpStatus}
+     * 
+     * @param httpStatus
+     *            标准HTTP状态码
+     */
     public WebServiceResponse(HttpStatus httpStatus) {
         this.data = null;
         this.dataSize = 0;
         this.setStatus(httpStatus);
     }
 
+    /**
+     * 新建空实体,并指定非标准HTTP状态码{@link WebServiceResponseStatus}
+     * 
+     * @param webServiceResponseStatus
+     *            非标准HTTP状态码
+     */
     public WebServiceResponse(WebServiceResponseStatus webServiceResponseStatus) {
         this.data = null;
         this.dataSize = 0;
