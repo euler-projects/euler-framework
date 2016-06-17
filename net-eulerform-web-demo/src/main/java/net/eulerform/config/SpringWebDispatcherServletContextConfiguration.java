572f229849868aebbb46d1881a93fe982e97e1c8
cFrost <cFrost@cfrost.net>
2016-06-18 01:44:46 +0800

    新功能
    1.jsp目录可配置
    2.非阻塞对象缓存
    3.优化JSP自动导入功能
    4.配置读取工具抛出异常改为Checked Exception

--- net-eulerform-web-demo/src/main/java/net/eulerform/config/SpringWebDispatcherServletContextConfiguration.java
+++ net-eulerform-web-demo/src/main/java/net/eulerform/config/SpringWebDispatcherServletContextConfiguration.java
@@ -1,7 +1,12 @@
 package net.eulerform.config;
 
+import net.eulerform.common.FilePathTool;
+import net.eulerform.common.GlobalProperties;
+import net.eulerform.common.GlobalPropertyReadException;
 import net.eulerform.web.core.annotation.WebController;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,18 +16,24 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(
-        basePackages = "net.eulerform.web.**.controller",
-        useDefaultFilters = false,
-        includeFilters = @ComponentScan.Filter(WebController.class)
-)
-public class SpringWebDispatcherServletContextConfiguration
-{
+@ComponentScan(basePackages = "net.eulerform.web.**.controller", useDefaultFilters = false, includeFilters = @ComponentScan.Filter(WebController.class))
+public class SpringWebDispatcherServletContextConfiguration {
+
+    private final Logger log = LogManager.getLogger();
+
     @Bean
-    public ViewResolver viewResolver(){
+    public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setViewClass(org.springframework.web.servlet.view.JstlView.class);
-        resolver.setPrefix("/WEB-INF/modulePages/");
+
+        String jspPath = "/WEB-INF/modulePages";
+        try {
+            jspPath = FilePathTool.changeToUnixFormat(GlobalProperties.get("web.jspPath"));
+        } catch (GlobalPropertyReadException e) {
+            log.info("Couldn't load web.jspPath , use '/WEB-INF/modulePages' for default.");
+        }
+
+        resolver.setPrefix(jspPath);
         resolver.setSuffix(".jsp");
         return resolver;
     }
