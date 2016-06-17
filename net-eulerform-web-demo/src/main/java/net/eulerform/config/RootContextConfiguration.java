5f56691d2ba68ae0afa1f374705196f09b2231ac
cFrost <cFrost@cfrost.net>
2016-06-18 02:27:01 +0800

    修改配置文件目录结构

--- net-eulerform-web-demo/src/main/java/net/eulerform/config/RootContextConfiguration.java
+++ net-eulerform-web-demo/src/main/java/net/eulerform/config/RootContextConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Controller;
 
 @Configuration
-@ImportResource({ "classpath*:beans.xml", "classpath:spring-security.xml" })
+@ImportResource({ "classpath*:config/beans.xml"})
 // @org.springframework.context.annotation.Import({SecurityConfiguration.class})
 @ComponentScan(basePackages = "net.eulerform.web", excludeFilters = @ComponentScan.Filter(Controller.class))
 
