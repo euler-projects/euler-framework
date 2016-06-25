6bf5f209c989d859bf24314313449ca2687eedcf
cFrost <cFrost@cfrost.net>
2016-06-25 21:41:52 +0800

    new node

--- net-eulerform-web-core/src/main/java/net/eulerform/web/core/filter/CrosFilter.java
+++ net-eulerform-web-core/src/main/java/net/eulerform/web/core/filter/CrosFilter.java
@@ -14,12 +14,14 @@ public class CrosFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-	    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-	    response.setHeader("Access-Control-Allow-Methods", request.getHeader("Access-Control-Request-Method"));
+        
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Methods", request.getHeader("Access-Control-Request-Method"));
         response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
-	    response.setHeader("Access-Control-Max-Age", "1800");
+        response.setHeader("Access-Control-Max-Age", "1800");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-	    filterChain.doFilter(request, response);
+        
+        filterChain.doFilter(request, response);
 	}
 
 }
