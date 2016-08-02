8453afb8713a3ce3698d82c44f4dcf369f53e0a0
cFrost <cFrost@cfrost.net>
2016-08-02 17:32:05 +0800

    修改模块名

---
+++ net-eulerform-web-oauth2-server/src/main/java/net/eulerform/web/module/oauth2/dao/IClientDao.java
@@ -0,0 +1,14 @@
+package net.eulerform.web.module.oauth2.dao;
+
+import net.eulerform.web.core.base.dao.IBaseDao;
+import net.eulerform.web.core.base.entity.PageResponse;
+import net.eulerform.web.core.base.entity.QueryRequest;
+import net.eulerform.web.module.oauth2.entity.Client;
+
+public interface IClientDao extends IBaseDao<Client> {
+
+    Client findClientByClientId(String clientId);
+
+    PageResponse<Client> findClientByPage(QueryRequest queryRequest, int pageIndex, int pageSize);
+
+}
---
+++ net-eulerform-web-oauth2-server/src/main/java/net/eulerform/web/module/oauth2/entity/GrantType.java
@@ -0,0 +1,20 @@
+package net.eulerform.web.module.oauth2.entity;
+
+public enum GrantType {
+    authorization_code,password,client_credentials,implicit,refresh_token;
+    
+    public static GrantType getGrantType(String str){
+        if(authorization_code.toString().equalsIgnoreCase(str)){
+            return authorization_code;
+        } else if(password.toString().equalsIgnoreCase(str)){
+            return password;
+        } else if(client_credentials.toString().equalsIgnoreCase(str)){
+            return client_credentials;
+        } else if(implicit.toString().equalsIgnoreCase(str)){
+            return implicit;
+        } else if(refresh_token.toString().equalsIgnoreCase(str)){
+            return refresh_token;
+        }
+        return null;
+    }
+}
