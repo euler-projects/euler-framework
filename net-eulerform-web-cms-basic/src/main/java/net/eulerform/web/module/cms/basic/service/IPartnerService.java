bfd1a4d0a80eb7b662bffcd3818f32d4c6adec26
cFrost <cFrost@cfrost.net>
2016-08-26 23:32:48 +0800

    权限规范

--- net-eulerform-web-cms-basic/src/main/java/net/eulerform/web/module/cms/basic/service/IPartnerService.java
+++ net-eulerform-web-cms-basic/src/main/java/net/eulerform/web/module/cms/basic/service/IPartnerService.java
@@ -9,19 +9,19 @@ import net.eulerform.web.core.base.entity.QueryRequest;
 import net.eulerform.web.core.base.service.IBaseService;
 import net.eulerform.web.module.cms.basic.entity.Partner;
 
+@PreAuthorize("isFullyAuthenticated() and hasAnyAuthority('CMS_ADMIN','ADMIN','SYSTEM')")
 public interface IPartnerService extends IBaseService {
-
+    
+    @PreAuthorize("permitAll")
     public PageResponse<Partner> findPartnerByPage(QueryRequest queryRequest, int pageIndex, int pageSize);
 
+    @PreAuthorize("permitAll")
     public List<Partner> loadPartners();
 
-    @PreAuthorize("isFullyAuthenticated() and hasAnyAuthority('ADMIN','DEMO','SYSTEM')")
     public void savePartner(Partner partner);
 
-    @PreAuthorize("isFullyAuthenticated() and hasAnyAuthority('ADMIN','DEMO','SYSTEM')")
     public void deleteLogo(String partnerId);
 
-    @PreAuthorize("isFullyAuthenticated() and hasAnyAuthority('ADMIN','DEMO','SYSTEM')")
     public void deletePartners(String[] idArray);
 
 }
--- net-eulerform-web-cms-basic/src/main/java/net/eulerform/web/module/cms/basic/service/ISlideshowService.java
+++ net-eulerform-web-cms-basic/src/main/java/net/eulerform/web/module/cms/basic/service/ISlideshowService.java
@@ -8,11 +8,12 @@ import org.springframework.web.multipart.MultipartFile;
 import net.eulerform.web.core.base.service.IBaseService;
 import net.eulerform.web.module.cms.basic.entity.Slideshow;
 
+@PreAuthorize("isFullyAuthenticated() and hasAnyAuthority('CMS_ADMIN','ADMIN','SYSTEM')")
 public interface ISlideshowService extends IBaseService {
 
+    @PreAuthorize("permitAll")
     public List<Slideshow> loadSlideshow();
 
-    @PreAuthorize("isFullyAuthenticated() and hasAnyAuthority('ADMIN','DEMO','SYSTEM')")
     public void saveSlideshow(List<MultipartFile> img, List<String> url);
 
 }
