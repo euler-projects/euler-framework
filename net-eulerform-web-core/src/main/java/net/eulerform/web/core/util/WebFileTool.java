0b1a5c1556ef2d32094907d190cade6bb862280c
cFrost <cFrost@cfrost.net>
2016-09-15 17:37:41 +0800

    some update

--- net-eulerform-web-core/src/main/java/net/eulerform/web/core/util/WebFileTool.java
+++ net-eulerform-web-core/src/main/java/net/eulerform/web/core/util/WebFileTool.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.ContextLoader;
 import org.springframework.web.multipart.MultipartFile;
 
 import net.eulerform.common.util.CalendarTool;
+import net.eulerform.common.util.FilePathTool;
 import net.eulerform.common.util.FileReader;
 import net.eulerform.web.core.exception.MultipartFileSaveException;
 
@@ -48,5 +49,45 @@ public class WebFileTool {
         } catch (IllegalStateException | IOException e) {
             throw new MultipartFileSaveException(e);
         }
+	}
+	
+	public static FileRet saveMultipartFile(MultipartFile multipartFile, String uploadPath, String fileName) throws MultipartFileSaveException{
+        try {
+            ServletContext scx = ContextLoader.getCurrentWebApplicationContext().getServletContext();
+
+            String realUploadPath = scx.getRealPath(uploadPath);
+            
+            realUploadPath = FilePathTool.changeToUnixFormat(realUploadPath);
+            
+            String perfix = CalendarTool.formatDate(new Date(), "yyyyMMddHHmmss-") + UUID.randomUUID().toString();
+            
+            String targetDirStr = realUploadPath + "/" + perfix;
+        
+            File targetDir = new File(targetDirStr);
+            
+            File targetFile = new File(targetDirStr, fileName);
+            if (!targetDir.exists()) {
+                targetDir.mkdirs();
+            }
+            multipartFile.transferTo(targetFile);
+            return new FileRet(perfix, targetFile);
+        } catch (IllegalStateException | IOException e) {
+            throw new MultipartFileSaveException(e);
+        }
+    }
+	
+	public static class FileRet {
+	    private String perfix;
+	    private File file;
+        public String getPerfix() {
+            return perfix;
+        }
+        public File getFile() {
+            return file;
+        }
+	    public FileRet(String perfix, File file) {
+	        this.perfix = perfix;
+	        this.file = file;
+	    }
 	}
 }
