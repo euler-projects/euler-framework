c38f1ac500a7f6a73fb8186b0fddf1f4d0cae8eb
cFrost <cFrost@eulerproject.io>
2017-01-06 16:56:35 +0800

    move datastore to basic

--- euler-web-basic/src/main/java/net/eulerframework/web/module/basic/controller/ErrorWebController.java
+++ euler-web-basic/src/main/java/net/eulerframework/web/module/basic/controller/ErrorWebController.java
@@ -3,7 +3,6 @@ package net.eulerframework.web.module.basic.controller;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import net.eulerframework.web.core.annotation.WebController;
 import net.eulerframework.web.core.base.controller.AbstractWebController;
@@ -13,7 +12,7 @@ import net.eulerframework.web.core.base.controller.AbstractWebController;
 @RequestMapping("/")
 public class ErrorWebController extends AbstractWebController {
     
-    @RequestMapping(value="error-{errorCode}", method = RequestMethod.GET)
+    @RequestMapping("error-{errorCode}")
     public String error(@PathVariable("errorCode") String errorCode) {
         return this.display(errorCode);
     }
