aa75c38ed0e5999bcad2d2769660c84a3b7932f1
cFrost <cFrost@eulerproject.io>
2017-02-23 17:05:13 +0800

    filename

--- euler-web-basic/src/main/java/net/eulerframework/web/module/file/entity/AbstractFileAttachment.java
+++ euler-web-basic/src/main/java/net/eulerframework/web/module/file/entity/AbstractFileAttachment.java
@@ -2,17 +2,26 @@ package net.eulerframework.web.module.file.entity;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
 
 @SuppressWarnings("serial")
 @MappedSuperclass
 public abstract class AbstractFileAttachment extends AbstractAttachment {
     @Column(name="FILE_ID", length=36, nullable=false)
     private String fileId;
+    @Transient
+    private String fileName;
     
     public String getFileId() {
         return fileId;
     }
     public void setFileId(String fileId) {
         this.fileId = fileId;
+    }
+    public String getFileName() {
+        return fileName;
+    }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
     }
 }
