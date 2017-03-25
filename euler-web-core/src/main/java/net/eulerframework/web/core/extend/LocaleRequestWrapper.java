39118e1399ff8f83f440e36f7970acb8576472a8
cFrost <cFrost@eulerproject.io>
2017-03-26 01:15:50 +0800

    close #9 test1

--- euler-web-core/src/main/java/net/eulerframework/web/core/extend/LocaleRequestWrapper.java
+++ euler-web-core/src/main/java/net/eulerframework/web/core/extend/LocaleRequestWrapper.java
@@ -12,50 +12,50 @@ import org.springframework.util.StringUtils;
 
 public class LocaleRequestWrapper extends HttpServletRequestWrapper {
     protected final Logger logger = LogManager.getLogger();
-    
+
     private final static String LOCALE_PARAM_NAME = "_locale";
     private final static String LOCALE_SESSION_ATTR_NAME = "__EULER_LOCALE__";
     private Locale locale;
 
     public LocaleRequestWrapper(HttpServletRequest request) {
         super(request);
-        
+
         try {
-        HttpSession session = request.getSession();
-        
-        if(session != null) {          
-            String localeParamValue = this.getRequest().getParameter(LOCALE_PARAM_NAME);
-            
-            if (StringUtils.hasText(localeParamValue)) {
-                Locale locale;
-                if(localeParamValue.indexOf('_') < 0) {          
-                    locale = new Locale(localeParamValue);
+            HttpSession session = request.getSession();
+
+            if (session != null) {
+                String localeParamValue = this.getRequest().getParameter(LOCALE_PARAM_NAME);
+
+                if (StringUtils.hasText(localeParamValue)) {
+                    Locale locale;
+                    if (localeParamValue.indexOf('_') < 0) {
+                        locale = new Locale(localeParamValue);
+                    } else {
+                        String[] localStr = localeParamValue.split("_");
+                        locale = new Locale(localStr[0], localStr[1]);
+                    }
+                    session.setAttribute(LOCALE_SESSION_ATTR_NAME, locale);
+                    this.locale = locale;
                 } else {
-                    String[] localStr = localeParamValue.split("_");                
-                    locale = new Locale(localStr[0], localStr[1]);                    
+                    Object locale = request.getSession().getAttribute(LOCALE_SESSION_ATTR_NAME);
+                    if (locale != null) {
+                        this.locale = (Locale) locale;
+                    } else {
+                        this.locale = request.getLocale();
+                    }
                 }
-                session.setAttribute(LOCALE_SESSION_ATTR_NAME, locale);
-                this.locale = locale;
             } else {
-                Object locale = request.getSession().getAttribute(LOCALE_SESSION_ATTR_NAME);
-                if(locale != null) {
-                    this.locale = (Locale) locale;
-                } else {
-                    this.locale = request.getLocale();                    
-                }
-            }           
-        } else {
-            this.locale = request.getLocale();
-        }
+                this.locale = request.getLocale();
+            }
         } catch (Exception e) {
             this.logger.error(e.getMessage(), e);
-            this.locale = request.getLocale();            
+            this.locale = request.getLocale();
         }
         request.setAttribute("__LOCALE", this.getLocale().toString());
     }
 
     @Override
-    public Locale getLocale() { 
-        return locale;  
-    } 
+    public Locale getLocale() {
+        return locale;
+    }
 }
