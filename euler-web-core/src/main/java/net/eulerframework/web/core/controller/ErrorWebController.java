05011759551695370784298f5774c9c072109156
cFrost <cFrost@eulerproject.io>
2017-03-31 22:48:48 +0800

    移除数据库驱动类配置

--- euler-web-core/src/main/java/net/eulerframework/web/core/controller/ErrorWebController.java
+++ euler-web-core/src/main/java/net/eulerframework/web/core/controller/ErrorWebController.java
@@ -1,4 +1,4 @@
-package net.eulerframework.web.module.basic.controller;
+package net.eulerframework.web.core.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
