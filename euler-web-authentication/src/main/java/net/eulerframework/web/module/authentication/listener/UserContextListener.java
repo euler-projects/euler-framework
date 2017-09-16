1ccff62a158a23f742157caa22acad78196ab5b8
cFrost <cFrost@eulerproject.io>
2017-09-16 11:54:23 +0800

    修改userContext的初始化方式

--- euler-web-authentication/src/main/java/net/eulerframework/web/module/authentication/listener/UserContextListener.java
+++ euler-web-authentication/src/main/java/net/eulerframework/web/module/authentication/listener/UserContextListener.java
@@ -6,15 +6,16 @@ import javax.servlet.ServletContextListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import net.eulerframework.common.base.log.LogSupport;
 import net.eulerframework.web.module.authentication.context.UserContext;
 import net.eulerframework.web.module.authentication.service.EulerUserDetailsService;
 
-public class UserContextListener implements ServletContextListener {
+public class UserContextListener extends LogSupport implements ServletContextListener {
     
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         WebApplicationContext rwp = WebApplicationContextUtils.getRequiredWebApplicationContext(sce.getServletContext());
-        EulerUserDetailsService userDetailsService= (EulerUserDetailsService)rwp.getBean("userDetailsService");
+        EulerUserDetailsService userDetailsService= rwp.getBean("userDetailsService", EulerUserDetailsService.class);
         UserContext.setUserDetailsServicel(userDetailsService);
     }
 
