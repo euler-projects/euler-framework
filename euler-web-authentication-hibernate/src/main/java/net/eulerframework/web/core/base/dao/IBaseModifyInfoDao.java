1d339f24bcb24a1711bfe78b68b3c8d6e40fc8a5
cfrost <cfrost@eulerproject.io>
2017-11-30 20:31:48 +0800

    del old codes

--- euler-web-authentication-hibernate/src/main/java/net/eulerframework/web/core/base/dao/IBaseModifyInfoDao.java
+++
@@ -1,6 +0,0 @@
-package net.eulerframework.web.core.base.dao;
-
-import net.eulerframework.web.core.base.entity.BaseModifyInfoEntity;
-
-public interface IBaseModifyInfoDao<T extends BaseModifyInfoEntity<?, ?>> extends IBaseDao<T>{
-}
--- euler-web-authentication-hibernate/src/main/java/net/eulerframework/web/module/authentication/dao/IUserProfileDao.java
+++
@@ -1,7 +0,0 @@
-package net.eulerframework.web.module.authentication.dao;
-
-import net.eulerframework.web.core.base.dao.IBaseDao;
-import net.eulerframework.web.module.authentication.entity.AbstractUserProfile;
-
-public interface IUserProfileDao<T extends AbstractUserProfile> extends IBaseDao<T> {
-}
