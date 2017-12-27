314f71bac6723ea7fc709e1d32878d4f01f112dc
cFrost <cFrost@eulerproject.io>
2017-12-27 23:34:43 +0800

    ynamic-assets

--- euler-web-core/src/main/java/net/eulerframework/web/core/controller/admin/IndexWebController.java
+++ euler-web-core/src/main/java/net/eulerframework/web/core/controller/admin/IndexWebController.java
@@ -3,10 +3,8 @@ package net.eulerframework.web.core.controller.admin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import net.eulerframework.web.config.WebConfig;
 import net.eulerframework.web.core.annotation.WebController;
 import net.eulerframework.web.core.base.controller.JspSupportWebController;
-import net.eulerframework.web.core.exception.PageNotFoundException;
 
 @WebController
 @RequestMapping("/")
@@ -16,15 +14,4 @@ public class IndexWebController extends JspSupportWebController {
     public String index() {
         return this.display("index");
     }
-    
-//    /**
-//     * 捕获所有未定义的请求，只有满足[contextPaht]/AdminRootPath才会被重定向到[contextPaht]/AdminRootPath/，其他请求返回404
-//     * @return 后台管理首页
-//     */
-//    @RequestMapping(value={"**"}, method = RequestMethod.GET)
-//    public String adminRedirect() {
-//        if(this.getRequest().getRequestURI().replace(this.getRequest().getContextPath(), "").equals(WebConfig.getAdminRootPath()))
-//            return this.redirect(WebConfig.getAdminRootPath() + "/");
-//        throw new PageNotFoundException(this.getRequest());
-//    }
 }
