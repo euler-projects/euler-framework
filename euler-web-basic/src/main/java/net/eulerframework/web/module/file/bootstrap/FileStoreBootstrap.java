c384620e0da4426d08bd67939bfaafb3bda48f5b
cFrost <cFrost@eulerproject.io>
2017-12-28 17:21:17 +0800

    file download path

--- euler-web-basic/src/main/java/net/eulerframework/web/module/file/bootstrap/FileStoreBootstrap.java
+++ euler-web-basic/src/main/java/net/eulerframework/web/module/file/bootstrap/FileStoreBootstrap.java
@@ -7,6 +7,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.web.WebApplicationInitializer;
 
 import net.eulerframework.common.base.log.LogSupport;
+import net.eulerframework.constant.EulerSysAttributes;
 import net.eulerframework.web.module.file.listener.FileStoreListener;
 
 @Order(1)
@@ -16,5 +17,12 @@ public class FileStoreBootstrap extends LogSupport implements WebApplicationInit
     public void onStartup(ServletContext container) throws ServletException {
         this.logger.info("Executing file store bootstrap.");
         container.addListener(new FileStoreListener());
+        
+        String contextPath = container.getContextPath();
+
+
+        container.setAttribute(EulerSysAttributes.FILE_DOWNLOAD_PATH_ATTR.value(), contextPath + "/file");
+        container.setAttribute(EulerSysAttributes.FILE_UPLOAD_ACTION_ATTR.value(), contextPath + "/uploadFile");
+
     }
 }
