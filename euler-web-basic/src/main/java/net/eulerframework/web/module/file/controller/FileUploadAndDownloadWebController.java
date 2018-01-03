7eb28b48bf78c14971ea3903e516dc57b567999a
cFrost <cFrost@eulerproject.io>
2018-01-03 23:59:23 +0800

    对于不支持的格式，下载原始图片

--- euler-web-basic/src/main/java/net/eulerframework/web/module/file/controller/FileUploadAndDownloadWebController.java
+++ euler-web-basic/src/main/java/net/eulerframework/web/module/file/controller/FileUploadAndDownloadWebController.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.annotation.Resource;
+import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
 
@@ -37,15 +38,12 @@ public class FileUploadAndDownloadWebController extends JspSupportWebController 
 
     @Resource
     private ArchivedFileService archivedFileService;
-    
+
     @ResponseBody
-    @RequestMapping(
-            path = {"file/{param}"}, 
-            method = RequestMethod.GET)
-    public void downloadArchivedFile(
-            @PathVariable("param") String param) throws IOException {
+    @RequestMapping(path = { "file/{param}" }, method = RequestMethod.GET)
+    public void downloadArchivedFile(@PathVariable("param") String param) throws IOException {
         ArchivedFile archivedFile = this.getRequestFile(param);
-        
+
         try {
             this.writeFile(archivedFile.getOriginalFilename(), archivedFile.getArchivedFile());
         } catch (FileNotFoundException e) {
@@ -55,115 +53,117 @@ public class FileUploadAndDownloadWebController extends JspSupportWebController 
             throw e;
         }
     }
-    
+
     @ResponseBody
     @RequestMapping(value = "image/{param}", method = RequestMethod.GET)
-    public void downloadImage(
-            @PathVariable("param") String param,
+    public void downloadImage(@PathVariable("param") String param,
             @RequestParam(required = false, defaultValue = "-1") int perfectWidth,
             @RequestParam(required = false, defaultValue = "-1") int perfectHeight) throws IOException {
-        
-        if(perfectHeight <= 0 && perfectWidth <= 0) {
+
+        if (perfectHeight <= 0 && perfectWidth <= 0) {
             this.downloadArchivedFile(param);
         } else {
-            ArchivedFile archivedFile = this.getRequestFile(param);
-            BufferedImage bufferedImage = ImageIO.read(archivedFile.getArchivedFile());
-
-            int originalHeight = bufferedImage.getHeight();
-            int originalWidth = bufferedImage.getWidth();
-
-            double scale= 1;
-            
-            if (perfectHeight <= 0 && perfectWidth <= 0){ // 高宽都不为正,不缩放
-                scale = 1;
-            } else if (perfectHeight <= 0){ // 高不为正,以宽为基准缩放
-                double scaleWidth = (double) perfectWidth / (double) originalWidth;
-                scale = scaleWidth;
-            } else if (perfectWidth <= 0){ // 宽不为正,以高为基准缩放
-                double scaleHeight = (double) perfectHeight / (double) originalHeight;
-                scale = scaleHeight;
-            } else { // 其他情况以缩放比较大的为基准
-                double scaleHeight = (double) perfectHeight / (double) originalHeight;
-                double scaleWidth = (double) perfectWidth / (double) originalWidth;
-                scale = scaleHeight > scaleWidth ? scaleHeight : scaleWidth;
-            }
-            
-            if(scale > 1) { //不允许放大
-                scale = 1;
-            }
-
             try {
-                if(scale == 1) {
-                    this.writeFile(archivedFile.getOriginalFilename(), archivedFile.getArchivedFile());
-                } else {
-                    this.writeImage(archivedFile.getOriginalFilename(), archivedFile.getArchivedFile(), scale);
+                ArchivedFile archivedFile = this.getRequestFile(param);
+                BufferedImage bufferedImage = ImageIO.read(archivedFile.getArchivedFile());
+
+                int originalHeight = bufferedImage.getHeight();
+                int originalWidth = bufferedImage.getWidth();
+
+                double scale = 1;
+
+                if (perfectHeight <= 0 && perfectWidth <= 0) { // 高宽都不为正,不缩放
+                    scale = 1;
+                } else if (perfectHeight <= 0) { // 高不为正,以宽为基准缩放
+                    double scaleWidth = (double) perfectWidth / (double) originalWidth;
+                    scale = scaleWidth;
+                } else if (perfectWidth <= 0) { // 宽不为正,以高为基准缩放
+                    double scaleHeight = (double) perfectHeight / (double) originalHeight;
+                    scale = scaleHeight;
+                } else { // 其他情况以缩放比较大的为基准
+                    double scaleHeight = (double) perfectHeight / (double) originalHeight;
+                    double scaleWidth = (double) perfectWidth / (double) originalWidth;
+                    scale = scaleHeight > scaleWidth ? scaleHeight : scaleWidth;
                 }
-            } catch (FileNotFoundException e) {
-                this.logger.warn(e.getMessage(), e);
-                throw new ResourceNotFoundException();
+
+                if (scale > 1) { // 不允许放大
+                    scale = 1;
+                }
+
+                try {
+                    if (scale == 1) {
+                        this.writeFile(archivedFile.getOriginalFilename(), archivedFile.getArchivedFile());
+                    } else {
+                        this.writeImage(archivedFile.getOriginalFilename(), archivedFile.getArchivedFile(), scale);
+                    }
+                } catch (FileNotFoundException e) {
+                    this.logger.warn(e.getMessage(), e);
+                    throw new ResourceNotFoundException();
+                } catch (IOException e) {
+                    throw e;
+                }
             } catch (IOException e) {
-                throw e;
+                this.downloadArchivedFile(param);
             }
         }
     }
-    
+
     private ArchivedFile getRequestFile(String requestParam) {
         String extensions = FileUtils.extractFileExtension(requestParam);
         String archivedFileId = FileUtils.extractFileNameWithoutExtension(requestParam);
         ArchivedFile archivedFile = this.archivedFileService.findArchivedFile(archivedFileId);
-        
-        if(archivedFile == null)
+
+        if (archivedFile == null)
             throw new ResourceNotFoundException();
-        
-        if(extensions != null) {
-            if(!extensions.equalsIgnoreCase(archivedFile.getExtension())) {
-                if(this.logger.isInfoEnabled()) {
-                    this.logger.info("The file extension does not match, specified as " + extensions + ", actually " + archivedFile.getExtension());
+
+        if (extensions != null) {
+            if (!extensions.equalsIgnoreCase(archivedFile.getExtension())) {
+                if (this.logger.isInfoEnabled()) {
+                    this.logger.info("The file extension does not match, specified as " + extensions + ", actually "
+                            + archivedFile.getExtension());
                 }
                 throw new ResourceNotFoundException();
             }
         }
 
         String archivedFilePath = WebConfig.getUploadPath();
-        
-        if(archivedFile.getArchivedPathSuffix() != null)
+
+        if (archivedFile.getArchivedPathSuffix() != null)
             archivedFilePath += "/" + archivedFile.getArchivedPathSuffix();
-        
+
         File file = new File(archivedFilePath, archivedFile.getArchivedFilename());
         archivedFile.setArchivedFile(file);
         return archivedFile;
     }
-    
+
     protected void writeImage(String fileName, File file, double scale) throws FileNotFoundException, IOException {
         HttpServletResponse response = this.getResponse();
         String extension = FileUtils.extractFileExtension(fileName);
         MIME mime;
-        if(StringUtils.hasText(extension)) {
+        if (StringUtils.hasText(extension)) {
             mime = MIMEUtils.getMIME(extension);
         } else {
             mime = MIMEUtils.getDefaultMIME();
         }
         this.getResponse().setHeader("Content-Type", mime.getContentType());
-        response.setHeader("Content-Disposition", mime.getContentDisposition() + 
-                ";fileName=\"" + new String(fileName.getBytes("utf-8"), "ISO8859-1") + "\"");
-        //response.setHeader("Transfer-Encoding", "chunked");
-        //response.setHeader("Content-Length", String.valueOf(file.length()));
-        
-        if(scale != 1) {
-            Thumbnails.of(file)
-            .scale(scale) 
-            .outputQuality(1f) 
-            .toOutputStream(response.getOutputStream());
+        response.setHeader("Content-Disposition", mime.getContentDisposition() + ";fileName=\""
+                + new String(fileName.getBytes("utf-8"), "ISO8859-1") + "\"");
+        // response.setHeader("Transfer-Encoding", "chunked");
+        // response.setHeader("Content-Length", String.valueOf(file.length()));
+
+        if (scale != 1) {
+            Thumbnails.of(file).scale(scale).outputQuality(1f).toOutputStream(response.getOutputStream());
         } else {
             SimpleFileIOUtils.readFileToOutputStream(file, response.getOutputStream(), 2048);
         }
-        
+
     }
-    
+
     @ResponseBody
     @RequestMapping(value = "uploadFile", method = RequestMethod.POST)
-    public EasyUIAjaxResponse<ArchivedFile> uploadArchivedFile(@RequestParam(value="file") MultipartFile multipartFile) throws FileArchiveException {
-        return new EasyUIAjaxResponse<>(this.archivedFileService.saveMultipartFile(multipartFile));        
+    public EasyUIAjaxResponse<ArchivedFile> uploadArchivedFile(
+            @RequestParam(value = "file") MultipartFile multipartFile) throws FileArchiveException {
+        return new EasyUIAjaxResponse<>(this.archivedFileService.saveMultipartFile(multipartFile));
     }
 
 }
