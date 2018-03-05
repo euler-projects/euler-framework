fed40fba2d11904805bf8aa8e38aa30b9f647024
cFrost <cFrost@eulerproject.io>
2018-03-05 08:39:05 +0800

    keep params in h redirect

--- euler-web-core/src/main/java/net/eulerframework/web/core/controller/StaticPageRedirectController.java
+++ euler-web-core/src/main/java/net/eulerframework/web/core/controller/StaticPageRedirectController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import net.eulerframework.common.util.CommonUtils;
 import net.eulerframework.web.config.WebConfig;
@@ -15,7 +16,10 @@ import net.eulerframework.web.core.base.controller.JspSupportWebController;
 public class StaticPageRedirectController extends JspSupportWebController {
     
     @RequestMapping("h/{url}")
-    public String error(@PathVariable("url") String url, Locale locale) {
+    public String error(@PathVariable("url") String url, RedirectAttributes redirectAttributes, Locale locale) {
+        
+        redirectAttributes.addAllAttributes(this.getRequest().getParameterMap());
+        
         return this.redirect(WebConfig.getStaticPagesRootPath() + "/" + CommonUtils.formatLocal(locale, '-') + "/" + url);
     }
     
