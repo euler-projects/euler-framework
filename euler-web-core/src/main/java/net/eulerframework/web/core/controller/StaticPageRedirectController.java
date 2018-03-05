6dd0c96e333340817f2d6476c4b10969e067ed3c
cFrost <cFrost@eulerproject.io>
2018-03-05 23:07:51 +0800

    拒绝中文乱吗 ->_->

--- euler-web-core/src/main/java/net/eulerframework/web/core/controller/StaticPageRedirectController.java
+++ euler-web-core/src/main/java/net/eulerframework/web/core/controller/StaticPageRedirectController.java
@@ -2,9 +2,9 @@ package net.eulerframework.web.core.controller;
 
 import java.util.Locale;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import net.eulerframework.common.util.CommonUtils;
 import net.eulerframework.web.config.WebConfig;
@@ -16,9 +16,13 @@ import net.eulerframework.web.core.base.controller.JspSupportWebController;
 public class StaticPageRedirectController extends JspSupportWebController {
     
     @RequestMapping("h/{url}")
-    public String error(@PathVariable("url") String url, RedirectAttributes redirectAttributes, Locale locale) {
+    public String error(@PathVariable("url") String url, Locale locale) {
         
-        redirectAttributes.addAllAttributes(this.getRequest().getParameterMap());
+        String queryString = this.getRequest().getQueryString();
+        
+        if(StringUtils.hasText(queryString)) {
+            url = url + "?" + queryString;
+        }
         
         return this.redirect(WebConfig.getStaticPagesRootPath() + "/" + CommonUtils.formatLocal(locale, '-') + "/" + url);
     }
