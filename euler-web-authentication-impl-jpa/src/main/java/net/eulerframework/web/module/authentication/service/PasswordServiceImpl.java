d4fc4e47e89e073fefd8f1cf4aaca5182f405b06
cFrost <cFrost@eulerproject.io>
2018-03-08 21:30:50 +0800

    reset password email gen

--- euler-web-authentication-impl-jpa/src/main/java/net/eulerframework/web/module/authentication/service/PasswordServiceImpl.java
+++ euler-web-authentication-impl-jpa/src/main/java/net/eulerframework/web/module/authentication/service/PasswordServiceImpl.java
@@ -31,11 +31,22 @@ package net.eulerframework.web.module.authentication.service;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
+import net.eulerframework.common.email.ThreadSimpleMailSender;
+import net.eulerframework.common.util.CommonUtils;
+import net.eulerframework.common.util.jwt.InvalidJwtException;
+import net.eulerframework.common.util.jwt.JwtEncryptor;
+import net.eulerframework.web.config.WebConfig;
+import net.eulerframework.web.module.authentication.entity.EulerUserEntity;
 import net.eulerframework.web.module.authentication.exception.InvalidEmailResetTokenException;
 import net.eulerframework.web.module.authentication.exception.InvalidSmsResetPinException;
+import net.eulerframework.web.module.authentication.exception.UserNotFoundException;
+import net.eulerframework.web.module.authentication.vo.UserResetJwtClaims;
+import net.eulerframework.web.util.ServletUtils;
 
 /**
  * @author cFrost
@@ -46,6 +57,10 @@ public class PasswordServiceImpl implements PasswordService {
     
     @Resource private PasswordEncoder passwordEncoder;
     @Resource private EulerUserEntityService eulerUserEntityService;
+    @Resource private JwtEncryptor jwtEncryptor;
+    
+    @Autowired(required = false)
+    private ThreadSimpleMailSender threadSimpleMailSender;
 
     @Override
     public PasswordEncoder getPasswordEncoder() {
@@ -64,7 +79,28 @@ public class PasswordServiceImpl implements PasswordService {
 
     @Override
     public void passwdResetEmailGen(String email) {
-        // TODO Auto-generated method stub
+        EulerUserEntity user;
+        try {
+            user = this.eulerUserEntityService.loadUserByEmail(email);
+        } catch (UserNotFoundException e) {
+            CommonUtils.sleep(1);
+            return;
+        }
+        
+        UserResetJwtClaims claims = new UserResetJwtClaims(user, 30 * 60);
+        Jwt jwt = this.jwtEncryptor.encode(claims);
+        String token = jwt.getEncoded();
+        String resetPasswordUrl = 
+                WebConfig.getWebUrl() 
+                + ServletUtils.getRequest().getContextPath() 
+                + "/reset-password?type=EMAIL&token="
+                + token;
+        String content = "<p>You can use the following link to reset your password in 10 minutes:</p><p><a href=\"" + resetPasswordUrl + "\">" + resetPasswordUrl + "</a></p>";
+        if(this.threadSimpleMailSender != null) {
+            this.threadSimpleMailSender.send("Reset Passwowrd", content, email);
+        } else {
+            System.out.println(content);
+        }
     }
 
     @Override
@@ -75,8 +111,13 @@ public class PasswordServiceImpl implements PasswordService {
 
     @Override
     public String analyzeUserIdFromEmailResetToken(String token) throws InvalidEmailResetTokenException {
-        // TODO Auto-generated method stub
-        throw new InvalidEmailResetTokenException();
+        UserResetJwtClaims claims;
+        try {
+            claims = this.jwtEncryptor.decode(token, UserResetJwtClaims.class);
+        } catch (InvalidJwtException e) {
+            throw new InvalidEmailResetTokenException();
+        }
+        return claims.getUserId();
     }
 
 }
