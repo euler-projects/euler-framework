e194d78145d2f6fc5bcd1938107a6821ac3bc544
cFrost <cFrost@eulerproject.io>
2018-08-07 23:53:19 +0800

    change license to Apache 2.0

--- euler-web-authentication/src/main/java/net/eulerframework/web/module/authentication/extend/EulerSmsCodeAuthenticator.java
+++ euler-web-authentication/src/main/java/net/eulerframework/web/module/authentication/extend/EulerSmsCodeAuthenticator.java
@@ -13,26 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.eulerframework.web.module.authentication.service;
-
-import java.util.Map;
+package net.eulerframework.web.module.authentication.extend;
 
 /**
  * @author cFrost
  *
  */
-public interface EulerUserExtraDataProcessor {
+public class EulerSmsCodeAuthenticator {
 
-    /**
-     * 处理用户的附加数据, 处理成功返回{@code true}, 失败返回{@code false}
-     * @param userId 对应用户的用户ID
-     * @param extraData 附加数据
-     * @return 处理成功返回{@code true}, 失败返回{@code false}
-     */
-    boolean process(String userId, Map<String, Object> extraData);
-
-    /**
-     * @param userId
-     */
-    Map<String, Object> loadExtraData(String userId);
 }
