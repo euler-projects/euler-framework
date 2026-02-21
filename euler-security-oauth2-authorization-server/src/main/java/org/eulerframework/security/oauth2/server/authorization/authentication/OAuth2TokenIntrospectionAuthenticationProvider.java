31e94426088c638c321f125e373927483a53f751
cFrost <cFrost@eulerproject.io>
2026-02-21 22:57:12 +0800

    refactor(authentication): 在Token Introspection响应中添加用户名及权限相关声明

--- euler-security-oauth2-authorization-server/src/main/java/org/eulerframework/security/oauth2/server/authorization/authentication/OAuth2TokenIntrospectionAuthenticationProvider.java
+++ euler-security-oauth2-authorization-server/src/main/java/org/eulerframework/security/oauth2/server/authorization/authentication/OAuth2TokenIntrospectionAuthenticationProvider.java
@@ -19,8 +19,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.oauth2.core.OAuth2TokenIntrospectionClaimNames;
@@ -37,6 +39,7 @@ import org.springframework.util.Assert;
 import org.springframework.util.CollectionUtils;
 
 import java.net.URL;
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -46,150 +49,164 @@ import java.util.Map;
  *
  * @author Gerardo Roza
  * @author Joe Grandja
- * @since 0.1.1
  * @see OAuth2TokenIntrospectionAuthenticationToken
  * @see RegisteredClientRepository
  * @see OAuth2AuthorizationService
  * @see <a target="_blank" href="https://tools.ietf.org/html/rfc7662#section-2.1">Section
  * 2.1 Introspection Request</a>
+ * @since 0.1.1
  */
 public final class OAuth2TokenIntrospectionAuthenticationProvider implements AuthenticationProvider {
 
-	private static final TypeDescriptor OBJECT_TYPE_DESCRIPTOR = TypeDescriptor.valueOf(Object.class);
+    private static final TypeDescriptor OBJECT_TYPE_DESCRIPTOR = TypeDescriptor.valueOf(Object.class);
 
-	private static final TypeDescriptor LIST_STRING_TYPE_DESCRIPTOR = TypeDescriptor.collection(List.class,
-			TypeDescriptor.valueOf(String.class));
+    private static final TypeDescriptor LIST_STRING_TYPE_DESCRIPTOR = TypeDescriptor.collection(List.class,
+            TypeDescriptor.valueOf(String.class));
 
-	private final Log logger = LogFactory.getLog(getClass());
+    private final Log logger = LogFactory.getLog(getClass());
 
-	private final RegisteredClientRepository registeredClientRepository;
+    private final RegisteredClientRepository registeredClientRepository;
 
-	private final OAuth2AuthorizationService authorizationService;
+    private final OAuth2AuthorizationService authorizationService;
 
-	/**
-	 * Constructs an {@code OAuth2TokenIntrospectionAuthenticationProvider} using the
-	 * provided parameters.
-	 * @param registeredClientRepository the repository of registered clients
-	 * @param authorizationService the authorization service
-	 */
-	public OAuth2TokenIntrospectionAuthenticationProvider(RegisteredClientRepository registeredClientRepository,
+    /**
+     * Constructs an {@code OAuth2TokenIntrospectionAuthenticationProvider} using the
+     * provided parameters.
+     *
+     * @param registeredClientRepository the repository of registered clients
+     * @param authorizationService       the authorization service
+     */
+    public OAuth2TokenIntrospectionAuthenticationProvider(RegisteredClientRepository registeredClientRepository,
                                                           OAuth2AuthorizationService authorizationService) {
-		Assert.notNull(registeredClientRepository, "registeredClientRepository cannot be null");
-		Assert.notNull(authorizationService, "authorizationService cannot be null");
-		this.registeredClientRepository = registeredClientRepository;
-		this.authorizationService = authorizationService;
-	}
+        Assert.notNull(registeredClientRepository, "registeredClientRepository cannot be null");
+        Assert.notNull(authorizationService, "authorizationService cannot be null");
+        this.registeredClientRepository = registeredClientRepository;
+        this.authorizationService = authorizationService;
+    }
 
-	@Override
-	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		OAuth2TokenIntrospectionAuthenticationToken tokenIntrospectionAuthentication = (OAuth2TokenIntrospectionAuthenticationToken) authentication;
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        OAuth2TokenIntrospectionAuthenticationToken tokenIntrospectionAuthentication = (OAuth2TokenIntrospectionAuthenticationToken) authentication;
 
-		OAuth2ClientAuthenticationToken clientPrincipal = OAuth2AuthenticationProviderUtilsAccessor
-				.getAuthenticatedClientElseThrowInvalidClient(tokenIntrospectionAuthentication);
+        OAuth2ClientAuthenticationToken clientPrincipal = OAuth2AuthenticationProviderUtilsAccessor
+                .getAuthenticatedClientElseThrowInvalidClient(tokenIntrospectionAuthentication);
 
-		OAuth2Authorization authorization = this.authorizationService
-			.findByToken(tokenIntrospectionAuthentication.getToken(), null);
-		if (authorization == null) {
-			if (this.logger.isTraceEnabled()) {
-				this.logger.trace("Did not authenticate token introspection request since token was not found");
-			}
-			// Return the authentication request when token not found
-			return tokenIntrospectionAuthentication;
-		}
+        OAuth2Authorization authorization = this.authorizationService
+                .findByToken(tokenIntrospectionAuthentication.getToken(), null);
+        if (authorization == null) {
+            if (this.logger.isTraceEnabled()) {
+                this.logger.trace("Did not authenticate token introspection request since token was not found");
+            }
+            // Return the authentication request when token not found
+            return tokenIntrospectionAuthentication;
+        }
 
-		if (this.logger.isTraceEnabled()) {
-			this.logger.trace("Retrieved authorization with token");
-		}
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace("Retrieved authorization with token");
+        }
 
-		OAuth2Authorization.Token<OAuth2Token> authorizedToken = authorization
-			.getToken(tokenIntrospectionAuthentication.getToken());
-		if (!authorizedToken.isActive()) {
-			if (this.logger.isTraceEnabled()) {
-				this.logger.trace("Did not introspect token since not active");
-			}
-			return new OAuth2TokenIntrospectionAuthenticationToken(tokenIntrospectionAuthentication.getToken(),
-					clientPrincipal, OAuth2TokenIntrospection.builder().build());
-		}
+        OAuth2Authorization.Token<OAuth2Token> authorizedToken = authorization
+                .getToken(tokenIntrospectionAuthentication.getToken());
+        if (!authorizedToken.isActive()) {
+            if (this.logger.isTraceEnabled()) {
+                this.logger.trace("Did not introspect token since not active");
+            }
+            return new OAuth2TokenIntrospectionAuthenticationToken(tokenIntrospectionAuthentication.getToken(),
+                    clientPrincipal, OAuth2TokenIntrospection.builder().build());
+        }
 
-		RegisteredClient authorizedClient = this.registeredClientRepository
-			.findById(authorization.getRegisteredClientId());
-		OAuth2TokenIntrospection tokenClaims = withActiveTokenClaims(authorizedToken, authorizedClient);
+        RegisteredClient authorizedClient = this.registeredClientRepository
+                .findById(authorization.getRegisteredClientId());
 
-		if (this.logger.isTraceEnabled()) {
-			this.logger.trace("Authenticated token introspection request");
-		}
+        Object principal = authorization.getAttribute(Principal.class.getName());
 
-		return new OAuth2TokenIntrospectionAuthenticationToken(authorizedToken.getToken().getTokenValue(),
-				clientPrincipal, tokenClaims);
-	}
+        OAuth2TokenIntrospection tokenClaims = withActiveTokenClaims(authorizedToken, authorizedClient, principal);
 
-	@Override
-	public boolean supports(Class<?> authentication) {
-		return OAuth2TokenIntrospectionAuthenticationToken.class.isAssignableFrom(authentication);
-	}
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace("Authenticated token introspection request");
+        }
 
-	private static OAuth2TokenIntrospection withActiveTokenClaims(
-			OAuth2Authorization.Token<OAuth2Token> authorizedToken, RegisteredClient authorizedClient) {
+        return new OAuth2TokenIntrospectionAuthenticationToken(authorizedToken.getToken().getTokenValue(),
+                clientPrincipal, tokenClaims);
+    }
 
-		OAuth2TokenIntrospection.Builder tokenClaims;
-		if (!CollectionUtils.isEmpty(authorizedToken.getClaims())) {
-			Map<String, Object> claims = convertClaimsIfNecessary(authorizedToken.getClaims());
-			tokenClaims = OAuth2TokenIntrospection.withClaims(claims).active(true);
-		}
-		else {
-			tokenClaims = OAuth2TokenIntrospection.builder(true);
-		}
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return OAuth2TokenIntrospectionAuthenticationToken.class.isAssignableFrom(authentication);
+    }
 
-		tokenClaims.clientId(authorizedClient.getClientId());
+    private static OAuth2TokenIntrospection withActiveTokenClaims(
+            OAuth2Authorization.Token<OAuth2Token> authorizedToken,
+            RegisteredClient authorizedClient,
+            Object principal) {
 
-		// TODO Set "username"
+        OAuth2TokenIntrospection.Builder tokenClaims;
+        if (!CollectionUtils.isEmpty(authorizedToken.getClaims())) {
+            Map<String, Object> claims = convertClaimsIfNecessary(authorizedToken.getClaims());
+            tokenClaims = OAuth2TokenIntrospection.withClaims(claims).active(true);
+        } else {
+            tokenClaims = OAuth2TokenIntrospection.builder(true);
+        }
 
-		OAuth2Token token = authorizedToken.getToken();
-		if (token.getIssuedAt() != null) {
-			tokenClaims.issuedAt(token.getIssuedAt());
-		}
-		if (token.getExpiresAt() != null) {
-			tokenClaims.expiresAt(token.getExpiresAt());
-		}
+        tokenClaims.clientId(authorizedClient.getClientId());
 
-		if (OAuth2AccessToken.class.isAssignableFrom(token.getClass())) {
-			OAuth2AccessToken accessToken = (OAuth2AccessToken) token;
-			tokenClaims.tokenType(accessToken.getTokenType().getValue());
-		}
+        if (principal instanceof UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
+                && usernamePasswordAuthenticationToken.getPrincipal() instanceof UserDetails userDetails) {
+            tokenClaims.username(userDetails.getUsername());
+            tokenClaims.claim("principal", userDetails);
+            tokenClaims.claim("authorities", userDetails.getAuthorities());
+        } else if (principal instanceof UserDetails userDetails) {
+            tokenClaims.username(userDetails.getUsername());
+            tokenClaims.claim("principal", userDetails);
+            tokenClaims.claim("authorities", userDetails.getAuthorities());
+        }
 
-		return tokenClaims.build();
-	}
+        OAuth2Token token = authorizedToken.getToken();
+        if (token.getIssuedAt() != null) {
+            tokenClaims.issuedAt(token.getIssuedAt());
+        }
+        if (token.getExpiresAt() != null) {
+            tokenClaims.expiresAt(token.getExpiresAt());
+        }
 
-	private static Map<String, Object> convertClaimsIfNecessary(Map<String, Object> claims) {
-		Map<String, Object> convertedClaims = new HashMap<>(claims);
+        if (OAuth2AccessToken.class.isAssignableFrom(token.getClass())) {
+            OAuth2AccessToken accessToken = (OAuth2AccessToken) token;
+            tokenClaims.tokenType(accessToken.getTokenType().getValue());
+        }
 
-		Object value = claims.get(OAuth2TokenIntrospectionClaimNames.ISS);
-		if (value != null && !(value instanceof URL)) {
-			URL convertedValue = ClaimConversionService.getSharedInstance().convert(value, URL.class);
-			if (convertedValue != null) {
-				convertedClaims.put(OAuth2TokenIntrospectionClaimNames.ISS, convertedValue);
-			}
-		}
+        return tokenClaims.build();
+    }
 
-		value = claims.get(OAuth2TokenIntrospectionClaimNames.SCOPE);
-		if (value != null && !(value instanceof List)) {
-			Object convertedValue = ClaimConversionService.getSharedInstance()
-				.convert(value, OBJECT_TYPE_DESCRIPTOR, LIST_STRING_TYPE_DESCRIPTOR);
-			if (convertedValue != null) {
-				convertedClaims.put(OAuth2TokenIntrospectionClaimNames.SCOPE, convertedValue);
-			}
-		}
+    private static Map<String, Object> convertClaimsIfNecessary(Map<String, Object> claims) {
+        Map<String, Object> convertedClaims = new HashMap<>(claims);
 
-		value = claims.get(OAuth2TokenIntrospectionClaimNames.AUD);
-		if (value != null && !(value instanceof List)) {
-			Object convertedValue = ClaimConversionService.getSharedInstance()
-				.convert(value, OBJECT_TYPE_DESCRIPTOR, LIST_STRING_TYPE_DESCRIPTOR);
-			if (convertedValue != null) {
-				convertedClaims.put(OAuth2TokenIntrospectionClaimNames.AUD, convertedValue);
-			}
-		}
+        Object value = claims.get(OAuth2TokenIntrospectionClaimNames.ISS);
+        if (value != null && !(value instanceof URL)) {
+            URL convertedValue = ClaimConversionService.getSharedInstance().convert(value, URL.class);
+            if (convertedValue != null) {
+                convertedClaims.put(OAuth2TokenIntrospectionClaimNames.ISS, convertedValue);
+            }
+        }
 
-		return convertedClaims;
-	}
+        value = claims.get(OAuth2TokenIntrospectionClaimNames.SCOPE);
+        if (value != null && !(value instanceof List)) {
+            Object convertedValue = ClaimConversionService.getSharedInstance()
+                    .convert(value, OBJECT_TYPE_DESCRIPTOR, LIST_STRING_TYPE_DESCRIPTOR);
+            if (convertedValue != null) {
+                convertedClaims.put(OAuth2TokenIntrospectionClaimNames.SCOPE, convertedValue);
+            }
+        }
+
+        value = claims.get(OAuth2TokenIntrospectionClaimNames.AUD);
+        if (value != null && !(value instanceof List)) {
+            Object convertedValue = ClaimConversionService.getSharedInstance()
+                    .convert(value, OBJECT_TYPE_DESCRIPTOR, LIST_STRING_TYPE_DESCRIPTOR);
+            if (convertedValue != null) {
+                convertedClaims.put(OAuth2TokenIntrospectionClaimNames.AUD, convertedValue);
+            }
+        }
+
+        return convertedClaims;
+    }
 
 }
