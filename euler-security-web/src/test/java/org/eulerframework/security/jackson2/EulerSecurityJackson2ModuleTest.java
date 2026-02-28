b7c5d09be99545f5e0598d6ec2587d7f90a68304
cFrost <cFrost@eulerproject.io>
2026-03-01 01:27:57 +0800

    fix: test

--- euler-security-web/src/test/java/org/eulerframework/security/jackson2/EulerSecurityJackson2ModuleTest.java
+++ euler-security-web/src/test/java/org/eulerframework/security/jackson2/EulerSecurityJackson2ModuleTest.java
@@ -17,25 +17,25 @@
 package org.eulerframework.security.jackson2;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eulerframework.security.core.userdetails.EulerUserDetails;
 import org.eulerframework.security.jackson.EulerSecurityJackson2Module;
-import org.springframework.security.jackson2.CoreJackson2Module;
+import org.springframework.security.jackson.CoreJacksonModule;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class EulerSecurityJackson2ModuleTest {
-    public static void main(String[] args) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new CoreJackson2Module());
-        mapper.registerModule(new EulerSecurityJackson2Module());
+    public static void main(String[] args) {
+        JsonMapper mapper = JsonMapper.builder()
+                .addModule(new CoreJacksonModule())
+                .addModule(new EulerSecurityJackson2Module())
+                .build();
         List<Object> rawList = new ArrayList<>();
-        rawList.add(new EulerUserDetails(EulerUserDetails.DEFAULT_TENANT_ID,"1", "1", "1", new ArrayList<>()));
+        rawList.add(new EulerUserDetails(EulerUserDetails.DEFAULT_TENANT_ID, "1", "1", "1", new ArrayList<>()));
         String json = mapper.writeValueAsString(rawList);
         System.out.println(json);
-        List<Object> readList = mapper.readValue(json, List.class);
+        List<?> readList = mapper.readValue(json, List.class);
         System.out.println(mapper.writeValueAsString(readList));
     }
 }
